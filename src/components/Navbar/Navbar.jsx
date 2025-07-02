e40133abd6854e7d8a7444c50000f3c232abd8df
sabiha-wasema <sabihawasemaa1244@gmail.com>
2025-07-02 13:30:14 +0600

    navbar initial design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,20 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <div>
-        <div>🍽️ FOODIED</div>
+      <div className="max-w-7xl mx-auto px-10 py-4 flex justify-between items-center">
         <div>
-          <Link to="/">Home</Link>
-          <Link to="/menu">Menu</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact"> contact</Link>
+          <Link
+            to=""
+            className="text-3xl font-bold text-amber-500 hover:text-amber-600 transition"
+          >
+            🍽️ FOODIED
+          </Link>
+        </div>
+        <div className="flex items-center space-x-8 ">
+          <Link to="/" className="text-gray-700 font-semibold hover:text-amber-500 transition">Home</Link>
+          <Link to="/menu" className="text-gray-700 font-semibold hover:text-amber-500 transition">Menu</Link>
+          <Link to="/about" className="text-gray-700 font-semibold hover:text-amber-500 transition">About</Link>
+          <Link to="/contact" className="text-gray-700 font-semibold hover:text-amber-500 transition"> contact</Link>
           <button>
             <FaSearch />
           </button>
