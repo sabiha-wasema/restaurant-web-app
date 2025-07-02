7cde46f8c6eaee22a5b33e3ef463a552b91d0dcb
sabiha-wasema <sabihawasemaa1244@gmail.com>
2025-07-02 13:52:43 +0600

    navbar initial design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,13 +19,22 @@ const Navbar = () => {
           <Link to="/menu" className="text-gray-700 font-semibold hover:text-amber-500 transition">Menu</Link>
           <Link to="/about" className="text-gray-700 font-semibold hover:text-amber-500 transition">About</Link>
           <Link to="/contact" className="text-gray-700 font-semibold hover:text-amber-500 transition"> contact</Link>
-          <button>
+          <button 
+          aria-label="Search"
+          className="text-xl ext-gray-700 font-semibold hover:text-amber-500 transition"
+          onClick={()=>alert("SearchBar Clicked!")}
+          >
             <FaSearch />
           </button>
-          <button>
-            <FaShoppingCart />
+          <button
+          aria-label="Cart"
+          className="text-gray-700 hover:text-blue-600 transition text-xl relative"
+            onClick={() => alert("Cart clicked!")}
+
+          >
+            <FaShoppingCart /> <span className="absolute -top-2 -right-2 bg-red-600 text-white rounded-full text-xs px-1">2</span>
           </button>
-          <Link to="">Sign Up</Link>
+          <Link to="/" className="ml-4 px-6 py-2 bg-white font-semibold text-gray-700 border-1 rounded-full hover:bg-gray-400 transition">Sign Up</Link>
         </div>
       </div>
     </nav>
