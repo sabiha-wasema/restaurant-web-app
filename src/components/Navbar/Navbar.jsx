431318aaf93c90a2d690f08a4875263fb6c87bca
sabiha-wasema <sabihawasemaa1244@gmail.com>
2025-07-02 13:17:57 +0600

    navbar initial design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
+  return (
+    <nav>
+      <div>
+        <div>🍽️ FOODIED</div>
         <div>
-            <h1>Navbar</h1>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact"> contact</Link>
+          <button>
+            <FaSearch />
+          </button>
+          <button>
+            <FaShoppingCart />
+          </button>
+          <Link to="">Sign Up</Link>
         </div>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
